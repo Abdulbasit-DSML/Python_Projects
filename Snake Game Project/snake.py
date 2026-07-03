ddf2e92f6c7e7645e9fe70be393099f0b028a7df
abdulbasit shaikh <316396basit@gmail.com>
2026-07-03 22:09:47 +0500

    Implement snake movement logic

--- Snake Game Project/snake.py
+++ Snake Game Project/snake.py
@@ -33,3 +33,11 @@ def reset(self):
         self.create_snake()
         self.head = self.segments[0]
 
+def move(self):
+
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
