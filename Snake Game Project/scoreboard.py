1fedb338f6fce04a0281a4a2827d7af4a02c22a7
abdulbasit shaikh <316396basit@gmail.com>
2026-07-12 23:42:26 +0500

    Update scoreboard.py

--- Snake Game Project/scoreboard.py
+++ Snake Game Project/scoreboard.py
@@ -20,4 +20,9 @@ def update_scoreboard(self):
             self.high_score = self.score
             with open("data.txt","w") as data:
                     data.write(f"{self.high_score}")
+         self.score = 0
+         self.update_scoreboard()
+
+
+
         
