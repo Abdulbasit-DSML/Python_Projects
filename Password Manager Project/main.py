0dc4f832e399abdb6f6db2408edf77160477f7e5
abdulbasit shaikh <316396basit@gmail.com>
2026-07-15 23:40:14 +0500

    Set up UI window for MyPass Manager

--- Password Manager Project/main.py
+++ Password Manager Project/main.py
@@ -4,3 +4,8 @@ BG_COLOR = "#F5F5DC"
 BTN_GEN_COLOR = "#FBC02D"
 BTN_ADD_COLOR = "#C62828"
 TEXT_COLOR = "#333333"
+
+# --- UI SETUP ---
+window = Tk()
+window.title("MyPass Manager")
+window.config(padx=50, pady=50, bg=BG_COLOR)
