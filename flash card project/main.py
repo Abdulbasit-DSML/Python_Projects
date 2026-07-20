6d6fb6c0d741177c6f9060f3605ec9c608267849
abdulbasit shaikh <316396basit@gmail.com>
2026-07-20 21:57:27 +0500

    Add canvas and card front image to main.py

--- flash card project/main.py
+++ flash card project/main.py
@@ -20,3 +20,5 @@ window = Tk()
 window.title("Flash-Card")
 
 window.config(padx=20, pady=20, bg=BACKGROUND_COLOR)
+canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_front_img = PhotoImage(file="images/card_front.png")
