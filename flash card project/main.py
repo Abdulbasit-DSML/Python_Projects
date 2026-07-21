83efae1434537e466956ee7fb07b8d0ba648344f
abdulbasit shaikh <316396basit@gmail.com>
2026-07-21 22:12:13 +0500

    Update main.py

--- flash card project/main.py
+++ flash card project/main.py
@@ -24,3 +24,4 @@ canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
